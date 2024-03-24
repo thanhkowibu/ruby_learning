ccaa927590866b4ee66a7aa9a94ffbaa7cc4a841
thanhkowibu <131281853+thanhkowibu@users.noreply.github.com>
2024-03-24 22:11:02 +0700

    changed file numbers_exercises.rb

---
+++ ruby.rb
@@ -0,0 +1,18 @@
+puts (1 + 1).odd?
+
+puts "Welcome " << "to " << "Odin!" 
+
+puts "hello"[0..4]
+
+puts "he77o".gsub("7", "l")
+
+puts "hello".insert(-2, " dude")
+
+puts '4' == 4
+
+animals = {:dog => 'barks', :cat => 'meows', :pig => 'oinks'}
+dictionary =  { one: "eins", two: "zwei", three: "drei" }
+puts animals[:cat]
+puts dictionary[:one]
+
+puts [1, 4, -6, 8, 2].sort
