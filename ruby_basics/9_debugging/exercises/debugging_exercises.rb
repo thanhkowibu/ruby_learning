ab4ec771a386578a537ecf0c153d0659b88a8fff
thanhkowibu <131281853+thanhkowibu@users.noreply.github.com>
2024-03-29 16:51:39 +0700

    completed 9_debugging

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -10,7 +10,7 @@ def decrement_smallest_value(nested_array)
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -24,7 +24,7 @@ def increment_greatest_value(nested_array)
   greatest_value = nested_array.flatten.min
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < current_value
         greatest_value = current_value
       end
     end
@@ -37,7 +37,8 @@ end
 
 def isogram?(string)
   original_length = string.length
-  string_array = string.downcase.split
+  string_array = string.downcase.split('')
+  p string_array
   unique_length = string_array.uniq.length
   original_length == unique_length
 end
@@ -48,9 +49,10 @@ end
 # First, include require 'pry-byebug' at the top of this page
 # Next insert plenty of breakpoints, and see if you can tell where things break
 # Once you find the error, fix it and get the test to pass
-
+require 'pry-byebug'
 def yell_greeting(string)
   name = string
-  name = name.downcase
+  name = name.upcase
+  binding.pry
   greeting = "WASSAP, #{name}!"
 end
